3defaca62bed9db4098763150e47f36bf436587a
Mariusz Wisniewski <lifcio@gmail.com>
2015-10-19 17:13:51 -0400

    Empty project

---
+++ SyncanoChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SyncanoChat
+//
+//  Created by Mariusz Wisniewski on 10/19/15.
+//  Copyright © 2015 Mariusz Wisniewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SyncanoChatTests/SyncanoChatTests.swift
@@ -0,0 +1,36 @@
+//
+//  SyncanoChatTests.swift
+//  SyncanoChatTests
+//
+//  Created by Mariusz Wisniewski on 10/19/15.
+//  Copyright © 2015 Mariusz Wisniewski. All rights reserved.
+//
+
+import XCTest
+@testable import SyncanoChat
+
+class SyncanoChatTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
